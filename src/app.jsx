43b1e92a017b7ce76a74c67a94459619a50876e9
Nakib Momin <nakibmomin@localadmins-MacBook-Pro-2.local>
2016-03-18 14:44:31 -0400

    Firebase integration

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+
+//Firebase DataSource
+var rootUrl = 'https://myreacttodo.firebaseio.com/';
 
 var Hello = React.createClass({
+  mixins: [ReactFire],
+  componentWillMount: function(){
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+  },
   render: function() {
+    console.log(this.state);
     return <h1 className="red">
-      Hello!
+      Hello Wolr!!
     </h1>
   }
 });
