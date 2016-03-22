dbb1426c086b4ef525c817216af50d5a0f3399ac
Nakib Momin <nakibmomin@localadmins-MacBook-Pro-2.local>
2016-03-22 16:04:13 -0400

    Delete todos that are marked complete

--- src/app.jsx
+++ src/app.jsx
@@ -20,9 +20,32 @@ var Hello = React.createClass({
     this.setState({loaded:true});
   },
   componentWillMount: function(){
-    fb = new Firebase(rootUrl + 'items/');
-    this.bindAsObject(fb, 'items');
-    fb.on('value', this.handleDataLoaded);
+    this.fb = new Firebase(rootUrl + 'items/');
+    this.bindAsObject(this.fb, 'items');
+    this.fb.on('value', this.handleDataLoaded);
+  },
+  onDeleteDoneClick: function(){
+    //Delete todo that are marked Done
+    for(var key in this.state.items){
+      if(this.state.items[key].done === true){
+        this.fb.child(key).remove();
+      }
+    }
+  },
+  deleteAllBtn: function(){
+    if(!this.state.loaded){
+      return
+    }else{
+      return <div className="text-center clear-complete">
+                <hr />
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  onClick={this.onDeleteDoneClick}
+                >Clear Complete
+                </button>
+             </div>
+    }
   },
   render: function() {
     return <div className="row panel panel-default">
@@ -34,6 +57,7 @@ var Hello = React.createClass({
                 <hr />
                 <div className={"content " + (this.state.loaded ? 'loaded' : '')}>
                   <List items= {this.state.items} />
+                  {this.deleteAllBtn()}
                 </div>
               </div>
            </div>
