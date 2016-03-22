5fb70bc1bdcec5fe09e312810941071175e0fe92
Nakib Momin <nakibmomin@localadmins-MacBook-Pro-2.local>
2016-03-22 14:33:08 -0400

    Updating todo done flag

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -1,15 +1,32 @@
 var React = require('react');
+var Firebase = require('firebase');
+
+//Firebase DataSource
+var rootUrl = 'https://myreacttodo.firebaseio.com/';
 
 module.exports = React.createClass({
   getInitialState: function(){
     return {
-      text: this.props.item.text
+      text: this.props.item.text,
+      done: this.props.item.done
     }
+  },
+  componentWillMount: function(){
+    this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key);
+  },
+  handleDoneChange: function(event){
+    var update = {done: event.target.checked}
+    this.setState(update);
+    this.fb.update(update);
   },
   render: function(){
     return <div className="input-group">
             <span className="input-group-addon">
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                onChange={this.handleDoneChange}
+                checked={this.state.done}
+                />
             </span>
              <input type="text" className="form-control" value={this.state.text} />
              <span className="input-group-btn">
